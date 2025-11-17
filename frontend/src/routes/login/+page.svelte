0b71a11184dc10b05a63ebfa889605c1464fa153
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2025-11-17 18:44:00 +0100

    :sparkles: Création de la page de login (début)

---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import imageFond from '$lib/images/register-image1.png';
+    import logo from '$lib/images/greenscore-logo.png';
+</script>
+
+<div class="flex items-center w-screen h-screen">
+    <img src="{imageFond}" alt="GreenScore" class="hidden xl:flex w-1/2 h-full object-cover">
+    <div class="w-full xl:w-1/2 h-full px-6 md:px-[120px] py-24 flex flex-col items-center justify-center">
+        <div class="flex flex-col w-full gap-10">
+            <div class="flex gap-2 items-center">
+                <img width="55px" height="auto" src="{logo}" alt="GreenScore Logo">
+                <h1 class="text-3xl font-outfit font-extrabold text-gs-green-950">GreenScore Web</h1>
+            </div>
+        </div>
+    </div>
+</div>
