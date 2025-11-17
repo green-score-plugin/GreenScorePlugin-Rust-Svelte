e42ad72192250c8dc37bbc247349763ff2b0f075
TistouCode <mathisrivrais@gmail.com>
2025-11-17 15:23:52 +0100

    :sparkles: Création du composant Header.svelte

---
+++ frontend/src/lib/Header.svelte
@@ -0,0 +1,16 @@
+
+<script>
+    import { onMount } from 'svelte';
+
+    let user = 10;
+    let profileMenuOpen = false;
+    let mobileMenuOpen = false;
+
+    function toggleProfileMenu() {
+        profileMenuOpen = !profileMenuOpen;
+    }
+
+    function toggleMobileMenu() {
+        mobileMenuOpen = !mobileMenuOpen;
+    }
+</script>
