4fb1073b5ba58452f2d0ffa4af711cfa3df83362
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2025-11-18 09:12:40 +0100

    :fire: suppression des fonctions de test de session

--- backend/src/controllers/home_controller.rs
+++ backend/src/controllers/home_controller.rs
@@ -4,14 +4,3 @@ use tower_sessions::Session;
 pub async fn index() -> Html<&'static str> {
     Html("<h1>Welcome to Axum!</h1>")
 }
-
-// Exemple d'utilisation de session
-pub async fn test_session(session: Session) -> String {
-    // Lire une valeur
-    let count: usize = session.get("count").await.unwrap().unwrap_or(0);
-
-    // Modifier la session
-    session.insert("count", count + 1).await.unwrap();
-
-    format!("Count: {}", count + 1)
-}
--- backend/src/router.rs
+++ backend/src/router.rs
@@ -5,5 +5,4 @@ use axum::routing::{get};
 pub fn create_router() -> Router {
     Router::new()
         .route("/", get(home_controller::index))
-        .route("/test_session", get(home_controller::test_session))
 }
