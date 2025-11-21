ddbd953eb052d5b1df03ed346fa29154c57be2f2
Theo2 <theobirembaux@gmail.com>
2025-11-21 15:15:59 +0100

    :stars: Début page mes infos

---
+++ frontend/src/routes/my_account/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    import MyInfo from './MyInfo.svelte';
+    import LeftMenu from './LeftMenu.svelte';
+    import salutation from '$lib/images/salutation.png';
+</script>
+
+
+
+<div  class="xl:px-52 flex flex-col">
+    <div class="px-4 lg:px-16 py-8 flex justify-center lg:justify-start items-center gap-x-4">
+        <img class="w-[54px] h-auto" src={salutation} alt="Salutation">
+        <h1 class="text-2xl font-bold">Bonjour !</h1>
+    </div>
+    <div class="flex flex-col lg:flex-row px-4 lg:px-16 gap-8 lg:gap-16 mb-2">
+        <div class="flex-grid h-fit min-w-60 shadow-lg bg-white items-center">
+            <LeftMenu />
+        </div>
+        <div class="shadow-lg bg-white py-4 px-6">
+            <MyInfo />
+        </div>
+        <div class="shadow-lg bg-white py-4 px-6">
+        </div>
+    </div>
+</div>
---
+++ frontend/src/routes/my_account/MyInfo.svelte
@@ -0,0 +1,37 @@
+<script>
+</script>
+
+<h1 class="text-2xl font-bold py-2">Mes informations</h1>
+<div class="flex gap-4 w-full text-grey-700 font-outfit font-semibold text-sm sm:flex-row">
+    <div class="w-full">
+        {{  }}
+        {{ }}
+    </div>
+</div>
+<div class="flex gap-4 w-full text-grey-700 font-outfit font-semibold text-sm sm:flex-row">
+    <div class="w-full">
+        {{  }}
+        {{  }}
+    </div>
+</div>
+
+<div class="flex gap-4 w-full text-grey-700 font-outfit font-semibold text-sm sm:flex-row">
+    <div class="w-full">
+        {{  }}
+        {{  }}
+
+    </div>
+</div>
+
+<div class="flex flex-col md:flex-row gap-4 w-full text-grey-700 font-outfit font-semibold text-sm sm:flex-row">
+    <div class="w-full">
+        {{ }}
+        {{  }}
+
+    </div>
+    <div class="w-full">
+    </div>
+</div>
+
+
+<button type="submit" class="w-full h-fit rounded-lg bg-gs-green-950 px-1 py-2 font-semibold font-outfit text-white">Valider</button>
