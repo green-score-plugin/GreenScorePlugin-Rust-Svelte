5e5d5a8fd909e93eeec67a1bed6e9c681b4e6c50
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2026-01-05 19:21:14 +0100

    :wrench: MAJ du fichier de config

--- plugin/src/config.js
+++ plugin/src/config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
     BACKEND: {
-      PLUGIN_BACKEND_URL: "http://127.0.0.1:8080/index.php", // URL du backend du plugin
+      PLUGIN_BACKEND_URL: "http://127.0.0.1:3000", // URL du backend du plugin
 
       DOMAIN: "127.0.0.1", // Utilisé pour récupérer l'ID utilisateur
       
-      WEBSITE_URL: "http://127.0.0.1:8000", // URL du site Greenscore
+      WEBSITE_URL: "http://127.0.0.1:3000", // URL du site Greenscore
       
-      LOGIN_URL: "http://127.0.0.1:8000/login", // URL de la page de connexion
-      DETAILS_URL: "http://127.0.0.1:8000/derniere-page-web-consultee" // URL de la page de détails
+      LOGIN_URL: "http://127.0.0.1:3000/login", // URL de la page de connexion
+      DETAILS_URL: "http://127.0.0.1:3000/derniere-page-web-consultee" // URL de la page de détails
     }
   };
   
