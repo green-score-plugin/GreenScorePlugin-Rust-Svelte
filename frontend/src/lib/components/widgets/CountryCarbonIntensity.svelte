ecd70448dfdff21352367cc034f67281d2670fcf
TistouCode <mathisrivrais@gmail.com>
2026-01-08 11:13:29 +0100

    :construction: Récupération des widgets

---
+++ frontend/src/lib/components/widgets/CountryCarbonIntensity.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    export let country: string | null = null;
+    export let flagUrl: string | null = null;
+    export let carbonIntensity: number | null = null;
+    import empreinteCarboneUrl from '$lib/images/empreinte_carbone.png';
+    import { tweened } from 'svelte/motion';
+    import { cubicOut } from 'svelte/easing';
+
+    const animatedValue = tweened(0, {
+        duration: 2000,
+        easing: cubicOut
+    });
+
+    $: if (carbonIntensity) {
+        animatedValue.set(carbonIntensity);
+    }
+</script>
+
+<div class="bg-white flex flex-col justify-between gap-4 p-6 rounded-lg shadow-md col-span-1 sm:col-span-2 order-2 sm:order-3 lg:col-span-4 lg:order-2">
+    {#if country && carbonIntensity}
+        <div class="flex items-center gap-4">
+            {#if flagUrl}
+                <img src="{ flagUrl }" alt="Drapeau de { country }" class="w-10 h-10 rounded-full object-cover border" loading="lazy">
+            {:else}
+                <div class="w-10 h-10 rounded-full bg-red-300"></div>
+            {/if}
+            <h2 class="text-lg font-bold font-outfit text-grey-950">Vous faites votre recherche depuis ce pays : { country }.</h2>
+        </div>
+        <div class="flex justify-center flex-wrap items-center gap-4">
+            <picture class="flex items-center justify-center mt-4 flex-1">
+                <img src="{empreinteCarboneUrl}" class="w-32 h-auto min-w-[100px]" alt="Empreinte Carbone" loading="lazy">
+            </picture>
+            <div class="flex flex-col justify-center items-center flex-1 font-outfit">
+                <p class="text-6xl font-bold text-[#92bfff] animate-counter" data-value="{carbonIntensity}">{Math.round($animatedValue)}</p>
+                <p class="text-xl font-bold">gCO2eq/kWh</p>
+                <p class="text-sm text-center pt-5">Votre empreinte carbone dépend aussi de l’intensité carbone du pays.</p>
+            </div>
+        </div>
+    {:else}
+        <p class="text-center text-gray-500 flex items-center justify-center h-full">pas de données</p>
+    {/if}
+</div>
---
+++ frontend/src/lib/components/widgets/Equivalent.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    export let equivalent: { name: string; value: number; icon: string | null } | null = null;
+    export let order: number = 1;
+
+    $: orderClasses = order === 1
+        ? 'order-4 sm:order-5 lg:order-4'
+        : 'order-5 sm:order-6 lg:order-6';
+</script>
+
+<div class="bg-white flex flex-col items-center justify-center p-6 rounded-lg shadow-md col-span-1 lg:col-span-3 text-grey-950 {orderClasses}">
+    {#if equivalent && equivalent.name && equivalent.value}
+        <div class="flex justify-center gap-2">
+            <picture class="bg-[#94e9b8] w-8 h-8 flex items-center justify-center rounded-full">
+                <img src="/images/equivalent.png" alt="Equivalent" class="w-4 h-4 rounded-full">
+            </picture>
+            <h2 class="text-lg font-bold font-outfit">Équivalent</h2>
+        </div>
+        {#if equivalent.icon}
+            <picture class="w-24 h-24 flex items-center justify-center mt-4">
+                <img
+                        src="/images/equivalents/{equivalent.icon}"
+                        alt={equivalent.name}
+                        class="w-full h-auto object-contain"
+                        loading="lazy"
+                >
+            </picture>
+        {/if}
+        <p class="text-center mt-2 text-lg font-medium font-outfit">
+            <span class="animate-counter">{equivalent.value}</span>
+            {equivalent.name}
+        </p>
+    {:else}
+        <p class="text-center text-gray-500 flex items-center justify-center h-full">pas de données</p>
+    {/if}
+</div>
