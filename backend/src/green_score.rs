fca9123d7f387950d4c1daefad186932e60ce32e
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2026-01-08 11:03:36 +0100

    :bug: Correction et optimisation de la fonction de calcul du GreenScore

--- backend/src/green_score.rs
+++ backend/src/green_score.rs
@@ -1,36 +1,23 @@
 pub fn calculate_green_score(carbon_footprint: f64) -> (String, String) {
     let echelle: f64 = 0.25;
-    let mut letter_green_score: String = String::new();
-    let mut env_nomination: String = String::new();
 
-    if carbon_footprint < echelle {
-        letter_green_score = "A".to_string();
-        env_nomination = "Maître des forêts".to_string();
-    }
-    else if carbon_footprint >= echelle && carbon_footprint < 2.0 * echelle {
-        letter_green_score = "B".to_string();
-        env_nomination = "Protecteur des Bois".to_string();
-    }
-    else if carbon_footprint >= 2.0 * echelle && carbon_footprint < 3.0 * echelle {
-        letter_green_score = "C".to_string();
-        env_nomination = "Frère des Arbres".to_string();
-    }
-    else if carbon_footprint >= 3.0 * echelle && carbon_footprint < 4.0 * echelle {
-        letter_green_score = "D".to_string();
-        env_nomination = "Initié de la Nature".to_string();
-    }
-    else if carbon_footprint >= 4.0 * echelle && carbon_footprint < 5.0 * echelle {
-        letter_green_score = "E".to_string();
-        env_nomination = "Explorateur Imprudent".to_string();
-    }
-    else if carbon_footprint >= 5.0 * echelle && carbon_footprint < 6.0 * echelle {
-        letter_green_score = "F".to_string();
-        env_nomination = "Tempête Numérique".to_string();
-    }
-    else if carbon_footprint >= 6.0 {
-        letter_green_score = "G".to_string();
-        env_nomination = "Desctructeur des Écosystèmes".to_string();
-    }
+    let (letter_green_score, env_nomination) = if carbon_footprint < echelle {
+        ("A", "Maître des forêts")
+    } else if carbon_footprint < 2.0 * echelle {
+        ("B", "Protecteur des Bois")
+    } else if carbon_footprint < 3.0 * echelle {
+        ("C", "Frère des Arbres")
+    } else if carbon_footprint < 4.0 * echelle {
+        ("D", "Initié de la Nature")
+    } else if carbon_footprint < 5.0 * echelle {
+        ("E", "Explorateur Imprudent")
+    } else if carbon_footprint < 6.0 * echelle {
+        ("F", "Tempête Numérique")
+    } else {
+        ("G", "Destructeur des Écosystèmes")
+    };
 
-    (letter_green_score, env_nomination)
+    eprintln!("Carbon footprint: {}, Green Score: {}, Nomination: {}", carbon_footprint, letter_green_score, env_nomination);
+
+    (letter_green_score.to_string(), env_nomination.to_string())
 }
