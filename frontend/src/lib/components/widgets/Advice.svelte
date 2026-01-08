fe6cb4045adc5f5ac65628fa144046a3b3bec4ea
TistouCode <mathisrivrais@gmail.com>
2026-01-08 11:11:22 +0100

    :construction: Récupération des widgets

---
+++ frontend/src/lib/components/widgets/Advice.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    export let advice: string | null = null;
+    export let type: 'dev' | 'nav' | null = null;
+</script>
+
+<div class="{type === 'dev' ? 'bg-[#92BFFF]' : 'bg-[#94E9B8]'} p-6 rounded-lg shadow-md col-span-1 lg:col-span-3 flex flex-col justify-between order-8 lg:order-8 font-outfit">
+    {#if advice && type}
+        <div class="flex gap-3 items-start">
+            <div class="bg-white w-8 h-8 min-w-8 min-h-8 flex items-center justify-center rounded-lg">
+                <i class="fa-solid fa-circle-question {type === 'dev' ? 'text-[#263346]' : 'text-gs-green-800'}"></i>
+            </div>
+            <h2 class="text-lg font-bold text-white">
+                Des conseils pour {type === 'dev' ? 'le développement' : 'la navigation'}&nbsp;?
+            </h2>
+        </div>
+        <p class="text-sm mt-2 text-white">
+            {advice}
+        </p>
+        <div class="flex w-full justify-end">
+            <button class="{type === 'dev' ? 'bg-[#455a79]' : 'bg-[#4b7d60]'} text-white px-4 py-2 mt-4 rounded-lg">En savoir plus</button>
+        </div>
+    {:else}
+        <p class="text-center text-gray-500 flex items-center justify-center h-full">pas de données</p>
+    {/if}
+</div>
---
+++ frontend/src/lib/components/widgets/BadgeGreenScore.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    export let letterGreenScore: string | null = 'A';
+    export let envNomination: string | null = null;
+
+    $: mobileUrl = letterGreenScore ? `/images/badges/mobile/${letterGreenScore}.png` : null;
+    $: desktopUrl = letterGreenScore ? `/images/badges/desktop/${letterGreenScore}.svg` : null;
+</script>
+
+<div class="flex flex-col bg-gradient-to-tr from-white via-blue-100 to-green-100 p-6 rounded-lg shadow-md col-span-1 order-3 sm:order-2 lg:order-3 lg:col-span-4 text-grey-950 font-outfit">
+    <div class="flex flex-col items-center justify-between h-full text-center">
+        <h2 class="text-lg font-bold row-span-1">Badge d'impact environnemental</h2>
+
+        {#if letterGreenScore}
+            <div class="flex-1 flex items-center justify-center w-full relative">
+                <picture class="w-full h-full flex items-center justify-center">
+                    <source media="(max-width: 768px)" srcset={mobileUrl} />
+                    <img
+                            src={desktopUrl}
+                            alt={`Badge Impact Environnemental ${letterGreenScore}`}
+                            class="w-[80%] h-auto object-contain"
+                            loading="lazy"
+                    />
+                </picture>
+            </div>
+        {:else}
+            <p class="text-center text-gray-500 flex items-center justify-center h-full">pas de données</p>
+        {/if}
+
+        {#if envNomination}
+            <p class="text-lg font-bold">{envNomination}</p>
+        {/if}
+    </div>
+</div>
