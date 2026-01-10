b8c76277baf7cf7429ff98459db576218275562c
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2026-01-10 13:09:25 +0100

    :construction: Réécriture du widget `ChartTop5PollutingSites`

---
+++ frontend/src/lib/components/widgets/ChartTop5PollutingSites.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let usersIdsCharts: string = '';
+</script>
+
+<div class="bg-white p-6 rounded-lg shadow-md col-span-1 sm:col-span-2 lg:col-span-6 flex flex-wrap min-h-[230px] order-7 lg:order-7 text-grey-950">
+    <div class="flex flex-col w-full h-auto">
+        <h2 class="text-lg font-bold mb-4">Top 5 des sites les plus polluants</h2>
+        <div class="relative w-full h-[230px]">
+            <canvas id="pollutionChart" data-endpoint="/api/top-sites?usersIds={ usersIdsCharts }"></canvas>
+        </div>
+    </div>
+</div>
