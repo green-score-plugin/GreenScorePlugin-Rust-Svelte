7b4b4c0ad03c51c3f53c6abc8ed2b86d1456ed49
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2026-01-12 17:56:23 +0100

    :bug: Correction de la route confidentialite sur le footer

--- frontend/src/lib/Footer.svelte
+++ frontend/src/lib/Footer.svelte
@@ -2,7 +2,7 @@
     <div class="container mx-auto text-center">
         <p class="text-lg mb-4">© 2025 GreenScore Web. Tous droits réservés.</p>
         <div class="space-x-4">
-            <a href="/politique-de-confidentialite" class="hover:text-blue-400">Politique de Confidentialité</a>
+            <a href="/confidentialite" class="hover:text-blue-400">Politique de Confidentialité</a>
             <span>|</span>
             <a href="/cgu" class="hover:text-blue-400">Conditions Générales d'Utilisation</a>
         </div>
