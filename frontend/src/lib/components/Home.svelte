27abc3b7e3dfac0c096e64cbdbe6c0626de5bc62
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2026-01-28 11:41:16 +0100

    :fire: Suppression de code dupliqué inutilement

--- frontend/src/lib/components/Home.svelte
+++ frontend/src/lib/components/Home.svelte
@@ -114,14 +114,7 @@
                 Chaque page que vous chargez consomme de l'énergie. Mesurez votre empreinte et agissez pour un internet plus vert.
             </p>
 
-            <div class="hidden lg:block">
-                <a href="https://addons.mozilla.org/fr/firefox/addon/greenscoreplugin/" class="inline-flex items-center bg-white text-gs-green-950 font-outfit font-regular p-3 rounded-full mx-auto shadow-md shadow-black">
-                    <img src="{firefoxLogo}" alt="Firefox" class="mr-2 h-6 w-6">
-                    Ajouter à Firefox
-                </a>
-            </div>
-
-            <div class="mt-8 lg:hidden flex justify-center">
+            <div class="mt-8 flex justify-center">
                 <a href="https://addons.mozilla.org/fr/firefox/addon/greenscoreplugin/" class="inline-flex items-center bg-white text-gs-green-950 font-outfit font-regular p-3 rounded-full mx-auto shadow-md shadow-black">
                     <img src="{firefoxLogo}" alt="Firefox" class="mr-2 h-6 w-6">
                     Ajouter à Firefox
