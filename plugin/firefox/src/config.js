0a6cbf775ba0d637bf29993a2fe996a4993f3e15
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2026-02-05 17:22:22 +0100

    :truck: Récupération de l'ancienne version du plugin pour Firefox

---
+++ plugin/firefox/src/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    BACKEND: {
+      PLUGIN_BACKEND_URL: "https://greenscore.alwaysdata.net/plugin", // URL du backend du plugin
+
+      DOMAIN: "greenscore.alwaysdata.net", // Utilisé pour récupérer l'ID utilisateur
+      
+      WEBSITE_URL: "https://greenscore.alwaysdata.net", // URL du site Greenscore
+      
+      LOGIN_URL: "https://greenscore.alwaysdata.net/login", // URL de la page de connexion
+      DETAILS_URL: "https://greenscore.alwaysdata.net/derniere-page-consultee" // URL de la page de détails
+    }
+  };
+  
+  if (typeof browser !== 'undefined') {
+  } else if (typeof module !== 'undefined') {
+    module.exports = CONFIG;
+  }
