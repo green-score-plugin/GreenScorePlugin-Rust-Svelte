915ef9935b5499a335db5f3252f51489747619b1
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2026-03-10 20:02:14 +0100

    :necktie: transformation du carbon footprint en kg

--- backend/src/service/equivalent_service.rs
+++ backend/src/service/equivalent_service.rs
@@ -7,8 +7,11 @@ impl EquivalentService {
         pool: &sqlx::MySqlPool,
         user_id: Option<i64>,
         n: i32,
-        carbon_footprint_in_kg: f64
+        carbon_footprint: f64
     ) -> Result<Vec<Equivalent>, sqlx::Error> {
+
+        let carbon_footprint_in_kg = carbon_footprint / 1000.0;
+
         match user_id {
             Some(id) => EquivalentRepository::get_n_user_equivalent(pool, id, n, carbon_footprint_in_kg).await,
             None => EquivalentRepository::get_n_equivalent(pool, n, carbon_footprint_in_kg).await,
