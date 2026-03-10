860e5c19517415d77b325b6a6e9e3c05e0b2dc23
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2026-03-10 19:56:32 +0100

    :recycle: refactor get_advice to use AdiceService directly

--- backend/src/controllers/home_controller.rs
+++ backend/src/controllers/home_controller.rs
@@ -3,6 +3,7 @@ use axum::Json;
 use serde::Serialize;
 use sqlx::MySqlPool;
 use crate::models::advice::Advice;
+use crate::service::advice_service::AdiceService;
 
 #[derive(Serialize)]
 pub struct AdviceResponse {
@@ -11,7 +12,7 @@ pub struct AdviceResponse {
 }
 
 pub async fn get_advice(State(pool): State<MySqlPool>) -> Json<AdviceResponse> {
-    let advice = match crate::service::advice_service::AdiceService::get_all_advice(&pool).await {
+    let advice = match AdiceService::get_all_advice(&pool).await {
         Ok(advice) => advice,
         Err(_) => return Json(AdviceResponse { success: false, advice: vec![] })
     };
