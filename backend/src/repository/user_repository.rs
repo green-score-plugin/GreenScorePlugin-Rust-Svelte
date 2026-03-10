6736ada6f759a3e6a3015bf3349ee863ecf29f25
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2026-03-10 14:01:47 +0100

    :recycle: erreur nom table et utilisation requête

--- backend/src/repository/user_repository.rs
+++ backend/src/repository/user_repository.rs
@@ -1,4 +1,4 @@
-use crate::models::user::User;
+use sqlx::Row;
 
 pub struct UserRepository;
 
@@ -8,47 +8,56 @@ impl UserRepository {
                              password_hash: &str,
                              first_name: &str,
                              last_name: &str) -> Result<i64, sqlx::Error> {
-        let result = sqlx::query!(
-            "INSERT INTO users (email, password_hash, first_name, last_name) VALUES (?, ?, ?, ?)"
+        let result = sqlx::query(
+            "INSERT INTO user (email, password, first_name, last_name) VALUES (?, ?, ?, ?)"
         )
-        .execute(pool)
         .bind(email)
         .bind(password_hash)
         .bind(first_name)
         .bind(last_name)
+        .execute(pool)
         .await?;
-        Ok(result.last_insert_id() as i64).expect("Failed to retrieve last insert ID");
+
+        Ok(result.last_insert_id() as i64)
     }
 
     pub async fn find_id_by_email(pool: &sqlx::MySqlPool, email: &str) -> Result<Option<i64>, sqlx::Error> {
-        let result = sqlx::query!(
-            "SELECT id FROM users WHERE email = ?",
-            email
+        let result = sqlx::query(
+            "SELECT id FROM user WHERE email = ?"
         )
+        .bind(email)
         .fetch_optional(pool)
         .await?;
 
-        Ok(result.map(|record| record.id))
+        Ok(result.map(|row| row.get::<i64, _>("id")))
     }
 
     pub async fn find_with_password_by_email(pool: &sqlx::MySqlPool, email: &str) -> Result<Option<(i64, String, String, String, Option<i64>, Option<i64>, bool)>, sqlx::Error> {
-        let result = sqlx::query!(
-            "SELECT id, password_hash, first_name, last_name, organisation_id, service_id ,est_admin FROM users WHERE email = ?"
+        let result = sqlx::query(
+            "SELECT id, password, first_name, last_name, organisation_id, service_id ,est_admin FROM user WHERE email = ?"
         )
-        .fetch_optional(pool)
         .bind(email)
+        .fetch_optional(pool)
         .await?;
 
-        Ok(result.map(|record| (record.id, record.password_hash, record.first_name, record.last_name, record.organisation_id, record.service_id, record.est_admin)))
+        Ok(result.map(|row| (
+            row.get::<i64, _>("id"),
+            row.get::<String, _>("password"),
+            row.get::<String, _>("first_name"),
+            row.get::<String, _>("last_name"),
+            row.get::<Option<i64>, _>("organisation_id"),
+            row.get::<Option<i64>, _>("service_id"),
+            row.get::<bool, _>("est_admin"),
+        )))
     }
 
     pub async fn join_organisation(pool: &sqlx::MySqlPool, user_id: i64, organisation_id: i64) -> Result<(), sqlx::Error> {
-        sqlx::query!(
-            "UPDATE users SET organisation_id = ? AND est_admin = true WHERE id = ?"
+        sqlx::query(
+            "UPDATE user SET organisation_id = ? AND est_admin = true WHERE id = ?"
         )
-        .execute(pool)
         .bind(organisation_id)
         .bind(user_id)
+        .execute(pool)
         .await?;
         Ok(())
     }
