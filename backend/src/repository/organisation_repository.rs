e706fe9f5b990f62073dececad5d26f04d33c6b6
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2026-03-10 14:12:53 +0100

    :recycle: erreur dans le nom de la table organisation et changement d'utilisation de sqlx

--- backend/src/repository/organisation_repository.rs
+++ backend/src/repository/organisation_repository.rs
@@ -1,14 +1,15 @@
 use crate::models::organisation::Organisation;
+use sqlx::Row;
+
 pub struct OrganisationRepository;
 
 impl OrganisationRepository {
 
     pub async fn find_by_id(pool: &sqlx::MySqlPool, organisation_id: i64) -> Result<Option<Organisation>, sqlx::Error> {
-        let organisation = sqlx::query_as!(
-            Organisation,
-            "SELECT id, organisation_name, organisation_code, siret FROM organisations WHERE id = ?",
-            organisation_id
+        let organisation = sqlx::query_as::<_, Organisation>(
+            "SELECT id, organisation_name, organisation_code, siret FROM organisation WHERE id = ?"
         )
+        .bind(organisation_id)
         .fetch_optional(pool)
         .await?;
 
@@ -16,29 +17,29 @@ impl OrganisationRepository {
     }
 
     pub async fn find_id_by_siret(pool: &sqlx::MySqlPool, siret: &str) -> Result<Option<i64>, sqlx::Error> {
-        let result = sqlx::query!(
-            "SELECT id FROM organisations WHERE siret = ?",
-            siret
+        let result = sqlx::query(
+            "SELECT id FROM organisation WHERE siret = ?"
         )
+        .bind(siret)
         .fetch_optional(pool)
         .await?;
 
-        Ok(result.map(|record| record.id))
+        Ok(result.map(|row| row.get::<i64, _>("id")))
     }
 
     pub async fn insert_organisation(pool: &sqlx::MySqlPool,
                                  organisation_name: &str,
                                  organisation_code: &str,
                                  siret: Option<&str>) -> Result<i64, sqlx::Error> {
-        let result = sqlx::query!(
-            "INSERT INTO organisations (organisation_name, organisation_code, siret) VALUES (?, ?, ?)"
+        let result = sqlx::query(
+            "INSERT INTO organisation (organisation_name, organisation_code, siret) VALUES (?, ?, ?)"
         )
-        .execute(pool)
         .bind(organisation_name)
         .bind(organisation_code)
         .bind(siret)
+        .execute(pool)
         .await?;
-        Ok(result.last_insert_id() as i64).expect("Failed to retrieve last insert ID");
+        Ok(result.last_insert_id() as i64)
     }
 
 }
