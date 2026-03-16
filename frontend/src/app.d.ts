852f3efb5ac49ef3da8940a0be8f962a8ef39e31
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2026-03-16 11:48:29 +0100

    :recycle: mise à jour du type de l'utilisateur dans l'interface Locals

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -4,7 +4,7 @@ declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
-			user?: import('$lib/types/account').Account;
+			user?: import('$lib/types/account').UserFull;
 		}
 		// interface PageData {}
 		// interface PageState {}
