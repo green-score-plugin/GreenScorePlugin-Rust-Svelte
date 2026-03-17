d46b90f55d9b2be4b9d567bc04ddc0df1b524dbc
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2026-03-17 12:42:29 +0100

    :lipstick: Correction des espacements entre les catégories du menu burger

--- frontend/src/lib/Header.svelte
+++ frontend/src/lib/Header.svelte
@@ -128,8 +128,8 @@
                 <li><a href="/derniere-page">{$t('header.last_visited_page')}</a></li>
 
                 {#if user}
-                    <li><a href="/mon-compte" class="block px-4 py-2 text-gray-800 hover:bg-gray-100">{$t('header.manage_account')}</a></li>
-                    <li><a href="/logout" class="block px-4 py-2 text-gray-800 hover:bg-gray-100">{$t('header.logout')}</a></li>
+                    <li><a href="/mon-compte">{$t('header.manage_account')}</a></li>
+                    <li><a href="/logout">{$t('header.logout')}</a></li>
                 {:else}
                     <li><a href="/login">{$t('header.login')}</a></li>
                 {/if}
