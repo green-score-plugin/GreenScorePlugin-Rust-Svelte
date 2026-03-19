88413b76c60f5bef52edd54600de5d33dbc6e80a
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2026-03-19 19:08:02 +0100

    :wrench: maj des fichiers de config pour le plugin

--- plugin/chrome/config.js
+++ plugin/chrome/config.js
@@ -1,13 +1,16 @@
+const BASE_DOMAIN = "greenscore.alwaysdata.net";
+const BASE_URL = `https://${BASE_DOMAIN}`;
+
 const CONFIG = {
     BACKEND: {
-      PLUGIN_BACKEND_URL: "https://greenscore.alwaysdata.net/plugin", // URL du backend du plugin
-
-      DOMAIN: "greenscore.alwaysdata.net", // Utilisé pour récupérer l'ID utilisateur
+      PLUGIN_BACKEND_URL: `${BASE_URL}/plugin`, // URL du backend du plugin
       
-      WEBSITE_URL: "https://greenscore.alwaysdata.net", // URL du site Greenscore
+      DOMAIN: BASE_DOMAIN, // Utilisé pour récupérer l'ID utilisateur
       
-      LOGIN_URL: "https://greenscore.alwaysdata.net/login", // URL de la page de connexion
-      DETAILS_URL: "https://greenscore.alwaysdata.net/derniere-page-consultee" // URL de la page de détails
+      WEBSITE_URL: BASE_URL, // URL du site Greenscore
+      
+      LOGIN_URL: `${BASE_URL}/login`, // URL de la page de connexion
+      DETAILS_URL: `${BASE_URL}/derniere-page-consultee` // URL de la page de détails
     }
   };
   
