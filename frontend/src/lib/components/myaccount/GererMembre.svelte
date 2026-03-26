31721347573fde253dd9c280b7ed9216f2c8dd2d
Conchez-Boueytou Robin <rcboueytou@iutbayonne.univ-pau.fr>
2026-03-26 20:18:11 +0100

    :bug: fix affichage admin quand l'utilisateur n'avais pas de service

--- frontend/src/lib/components/myaccount/GererMembre.svelte
+++ frontend/src/lib/components/myaccount/GererMembre.svelte
@@ -149,7 +149,7 @@
                                             Admin
                                         </span>
                                     {/if}
-                                {:else}
+                                {:else if user.id === member.id}
                                     <span
                                         class="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-blue-100 text-blue-800 transition-colors duration-200"
                                         title="admin"
